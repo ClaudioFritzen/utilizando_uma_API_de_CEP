ab25397ed31e70480da4114174725ee1c575ff33
ClaudioFritzen <sdfritzen96@gmail.com>
2022-12-16 00:34:53 +0000

    funcionando o basico

--- API/api.py
+++ API/api.py
@@ -1,7 +1,19 @@
 import requests
 
 cpf = '93806832'
+cpf1 = '93821248'
 
-link = f'http://viacep.com.br/ws/{cpf}/json/'
+link = f'https://viacep.com.br/ws/{cpf1}/json/'
 
-print(link)
+requisicao = requests.get(link)
+
+#print(requisicao)
+
+json_cep = requisicao.json()
+print(json_cep)
+
+uf = json_cep['uf']
+rua = json_cep['logradouro']
+bairro = json_cep['bairro']
+cidade = json_cep['localidade']
+print(uf, rua, bairro)
