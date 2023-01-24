221f943d9d39176b01e411118c12fe088eeb4265
ClaudioFritzen <sdfritzen96@gmail.com>
2023-01-24 22:13:16 +0000

    criamos uma função dentro do views para exibir o html

--- apiweb/views.py
+++ apiweb/views.py
@@ -4,4 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 def Consulta_cep(request):
     if request.method == 'GET':
-        return HttpResponse ('Digite seu Cep <input value="seu cep">')
+        return HttpResponse ("""<form>
+  <label for="cep">CEP:</label>
+  <input type="text" id="cep" name="cep">
+  <br>
+  <label for="rua">Rua:</label>
+  <input type="text" id="rua" name="rua" disabled>
+  <br>
+  <label for="bairro">Bairro:</label>
+  <input type="text" id="bairro" name="bairro" disabled>
+</form>
+""")
